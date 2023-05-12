30753ffa0d007cef9d8f24ae8f12933db3f97d4e
Phạm Gia Đạt <84780233+codestone23@users.noreply.github.com>
2023-05-13 02:21:09 +0700

    add function transfer product

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,12 +12,135 @@ const MY_SHOPPE = {
     transferLogin: $(".auth-form__transfer-login"),
     transferRegister: $(".auth-form__transfer-register"),
     cancelForm: $$(".auth-form__controls-back"),
+    btnHomeFilter: $$(".home-filter .btn"),
+    btnPopular: $(".home-filter__popular"),
+    btnBestSeller: $(".home-filter__bestseller"),
+    btnLatest: $(".home-filter__latest"),
+    proPopular: $$(".popular--active"),
+    proLatest: $$(".latest--active"),
+    proBestSeller: $$(".bestseller--active"),
+    itemProduct: $$(".home-product .js-product"),
+    headerItem: $$(".header__sort-bar .header__sort-item"),
+    headerPopular: $(".header__sort-item-popular"),
+    headerLatest: $(".header__sort-item-latest"),
+    headerBest: $(".header__sort-item-bestseller"),
     start: function(){
         this.handleRegister();
         this.handleLogin();
         this.handleTransferRegister();
         this.handleTransferLogin();
         this.handleCancelForm();
+        this.handleTransferProductPopular();
+        this.handleTransferProductLatest();
+        this.handleTransferProductBestseller();
+    },
+    handleTransferProductLatest: function(){
+        this.btnLatest.onclick = () =>{
+            if(this.btnLatest.classList.contains("btn--active")) return;
+            this.btnHomeFilter.forEach((item)=>{
+                item.classList.remove("btn--active");
+            })
+            this.btnLatest.classList.add("btn--active");
+            this.itemProduct.forEach((item)=>{
+                item.classList.remove("active");
+            })
+            this.proLatest.forEach((item)=>{
+                item.classList.add("active");
+            })
+            this.headerItem.forEach((item)=>{
+                item.classList.remove("header__sort-item--active")
+            })
+            this.headerLatest.classList.add("header__sort-item--active");
+        }
+        this.headerLatest.onclick = () =>{
+            if(this.headerLatest.classList.contains("header__sort-item--active")) return;
+            this.headerItem.forEach((item)=>{
+                item.classList.remove("header__sort-item--active");
+            })
+            this.headerLatest.classList.add("header__sort-item--active");
+            this.itemProduct.forEach((item)=>{
+                item.classList.remove("active");
+            })
+            this.proLatest.forEach((item)=>{
+                item.classList.add("active");
+            })
+            this.btnHomeFilter.forEach((item)=>{
+                item.classList.remove("btn--active")
+            })
+            this.btnLatest.classList.add("btn--active");
+        }
+    },
+    handleTransferProductPopular: function(){
+        this.btnPopular.onclick = () =>{
+            if(this.btnPopular.classList.contains("btn--active")) return;
+            this.btnHomeFilter.forEach((item)=>{
+                item.classList.remove("btn--active");
+            })
+            this.btnPopular.classList.add("btn--active");
+            this.itemProduct.forEach((item)=>{
+                item.classList.remove("active");
+            })
+            this.proPopular.forEach((item)=>{
+                item.classList.add("active");
+            })
+            this.headerItem.forEach((item)=>{
+                item.classList.remove("header__sort-item--active")
+            })
+            this.headerPopular.classList.add("header__sort-item--active");
+        }
+        this.headerPopular.onclick = () =>{
+            if(this.headerPopular.classList.contains("header__sort-item--active")) return;
+            this.headerItem.forEach((item)=>{
+                item.classList.remove("header__sort-item--active");
+            })
+            this.headerPopular.classList.add("header__sort-item--active");
+            this.itemProduct.forEach((item)=>{
+                item.classList.remove("active");
+            })
+            this.proPopular.forEach((item)=>{
+                item.classList.add("active");
+            })
+            this.btnHomeFilter.forEach((item)=>{
+                item.classList.remove("btn--active")
+            })
+            this.btnPopular.classList.add("btn--active");
+        }
+    },
+    handleTransferProductBestseller: function(){
+        this.btnBestSeller.onclick = () =>{
+            if(this.btnBestSeller.classList.contains("btn--active")) return;
+            this.btnHomeFilter.forEach((item)=>{
+                item.classList.remove("btn--active");
+            })
+            this.btnBestSeller.classList.add("btn--active");
+            this.itemProduct.forEach((item)=>{
+                item.classList.remove("active");
+            })
+            this.proBestSeller.forEach((item)=>{
+                item.classList.add("active");
+            })
+            this.headerItem.forEach((item)=>{
+                item.classList.remove("header__sort-item--active")
+            })
+            this.headerBest.classList.add("header__sort-item--active");
+        }
+        this.headerBest.onclick = () =>{
+            if(this.headerBest.classList.contains("header__sort-item--active")) return;
+            this.headerItem.forEach((item)=>{
+                item.classList.remove("header__sort-item--active");
+            })
+            this.headerBest.classList.add("header__sort-item--active");
+            this.itemProduct.forEach((item)=>{
+                item.classList.remove("active");
+            })
+            this.proBestSeller.forEach((item)=>{
+                item.classList.add("active");
+            })
+            this.btnHomeFilter.forEach((item)=>{
+                item.classList.remove("btn--active")
+            })
+            this.btnBestSeller.classList.add("btn--active");
+        }
     },
     handleRegister : function(){
         this.nodeRegister.onclick = () =>{
